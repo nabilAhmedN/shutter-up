f3990e96a7103a0504ca6a314e30ab52189411b3
nabilAhmedN <nabilahmednahid@gmail.com>
2022-11-07 23:06:47 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDJ0SQJGpo74z406uX89e6EvBKAzSQinys",
+    authDomain: "shutter-up-12025.firebaseapp.com",
+    projectId: "shutter-up-12025",
+    storageBucket: "shutter-up-12025.appspot.com",
+    messagingSenderId: "333703549066",
+    appId: "1:333703549066:web:0cd0096f889628f79f1d73",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
